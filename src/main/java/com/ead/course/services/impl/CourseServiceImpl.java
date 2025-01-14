0e5cee44a4e9cf48ffea4f16200c5dea4c6f42c0
Yure Moutella <ymoutella@gmail.com>
2025-01-14 15:31:55 -0300

    service implementation

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,6 +1,15 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.repository.ModuleRepository;
 import com.ead.course.services.CourseService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CourseServiceImpl implements CourseService {
+
+  @Autowired
+  ModuleRepository moduleRepository;
+
 }
