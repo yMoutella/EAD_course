8eee058854742fe05e5ccd4e6921882c6c768af0
Yure Moutella <ymoutella@gmail.com>
2025-01-17 01:14:43 -0300

    changes

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,4 +1,9 @@
 package com.ead.course.services;
 
+import java.util.List;
+
+import com.ead.course.models.CourseModel;
+
 public interface CourseService {
+	List<CourseModel> findAll();
 }
--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -3,13 +3,20 @@ package com.ead.course.services.impl;
 import com.ead.course.repository.ModuleRepository;
 import com.ead.course.services.CourseService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.ead.course.models.CourseModel;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
   @Autowired
   ModuleRepository moduleRepository;
+
+  public List<CourseModel> findAll() {
+    return moduleRepository.findAll();
+  }
 
 }
